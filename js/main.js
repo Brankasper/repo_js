f557f532a9fe82c89a64854b77cf62bd9992c9ee
Brandon <brandon.carnagli@gmail.com>
2022-01-23 14:27:17 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-alert();
+//alert();
 
-const NOMBRE = "brandon";
+/*const NOMBRE = "brandon";
 const BLANCO = " ";
-let EDAD = 28;
+let edad = 28;
 
-let resultado = NOMBRE + EDAD;
+let resultado = NOMBRE + BLANCO + edad;
+
+edad = edad + 1;
+
+resultado = NOMBRE + BLANCO + edad;*/
+
+let nombre = prompt("ingrese su nombre");
+
+let edad = prompt("ingrese edad");
+
+console.log("esto lo puse fijo");
+
+console.log(1 + 1);
+
+console.log(nombre);
+console.log(edad);
+console.log(nombre + " " + edad);
+
+alert("esto lo puse fijo");
+
+alert(1 + 1);
+
+alert(nombre);
+alert(edad);
+alert(nombre + " " + edad);
+
+document.write("esto lo puse fijo");
+
+document.write(1 + 1);
+
+document.write(nombre);
+document.write(edad);
+document.write(nombre + " " + edad);
